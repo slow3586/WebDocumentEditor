d97f0f804d2d936c2d6172504d1c65c28c64a222
slow3586 <6337520+slow3586@users.noreply.github.com>
2022-03-09 17:28:20 +0700

    more js updates

--- src/main/webapp/static/js/dojoscript.js
+++ src/main/webapp/static/js/dojoscript.js
@@ -1,25 +1,77 @@
 define([
-    "dojo/dom", "dojo/on", "dojo/_base/window", "dojo/store/Memory",
-    "dijit/tree/ObjectStoreModel", "dijit/Tree", "dijit/layout/TabContainer", "dijit/layout/ContentPane", "dijit/layout/BorderContainer",
-	"dijit/MenuBar",
-    "dijit/PopupMenuBarItem",
-    "dijit/Menu",
-    "dijit/MenuItem",
-    "dijit/DropDownMenu",
-	"dijit/Toolbar",
-    "dijit/form/Button",
-    "dojo/_base/array",
-	"dijit/Editor",
-	"dijit/_editor/plugins/AlwaysShowToolbar",
-	"dijit/Dialog", 
-	"dijit/form/ValidationTextBox",
-	"dojo/request",
+        "dojo/_base/kernel",
+        "dojo/dom", 
+        "dojo/on", 
+        "dojo/_base/window", 
+        "dojo/store/Memory",
+        "dijit/tree/ObjectStoreModel", 
+        "dijit/Tree", 
+        "dijit/layout/TabContainer", 
+        "dijit/layout/ContentPane", 
+        "dijit/layout/BorderContainer",
+        "dijit/MenuBar",
+        "dijit/PopupMenuBarItem",
+        "dijit/Menu",
+        "dijit/MenuItem",
+        "dijit/DropDownMenu",
+        "dijit/Toolbar",
+        "dijit/form/Button",
+        "dojo/_base/array",
+        "dijit/Editor",
+        "dijit/_editor/plugins/AlwaysShowToolbar",
+        "dijit/Dialog", 
+        "dijit/form/ValidationTextBox",
+        "dojo/request",
         'dgrid/OnDemandGrid',
         'dstore/RequestMemory',
         'dstore/Rest',
-    "dojo/domReady!"
-], function(dom, on, win, Memory, ObjectStoreModel, Tree, TabContainer, ContentPane, BorderContainer, MenuBar, PopupMenuBarItem, Menu, MenuItem, DropDownMenu, Toolbar, Button, array, Editor, AlwaysShowToolbar, Dialog, ValidationTextBox, request, OnDemandGrid, RequestMemory, Rest){
-    
+        "dojo/domReady!"
+], function(
+        kernel, 
+        dom, 
+        on, 
+        win,
+        Memory, 
+        ObjectStoreModel, 
+        Tree, 
+        TabContainer, 
+        ContentPane, 
+        BorderContainer, 
+        MenuBar,
+        PopupMenuBarItem,
+        Menu, 
+        MenuItem,
+        DropDownMenu, 
+        Toolbar, 
+        Button, 
+        array, 
+        Editor,
+        AlwaysShowToolbar,
+        Dialog, 
+        ValidationTextBox,
+        request, 
+        OnDemandGrid, 
+        RequestMemory, 
+        Rest
+){
+
+    function createBorderContainer(){
+        //Init
+        kernel.global.mainBorderContainer = new BorderContainer({style: "height: 100%; width:100%;", liveSplitters:true, gutter:true});
+        
+        createMainMenuBar();
+        
+        //Main tab container
+        kernel.global.mainTabContainer = new TabContainer({region: "center"});
+        kernel.global.mainBorderContainer.addChild(kernel.global.mainTabContainer);
+
+        createTreeMenu();
+        
+        //Startup
+        document.body.appendChild(kernel.global.mainBorderContainer.domNode);
+        kernel.global.mainBorderContainer.startup();
+    };
+
     function createMainMenuBar(){
         var menu = new MenuBar({region: "top"});
         var subMenu = new DropDownMenu({});
@@ -33,46 +85,10 @@ define([
             label: "Language",
             popup: subMenu
         }));
-        return menu;
+        
+        kernel.global.mainBorderContainer.addChild(menu);
     };
     
-    function openEditAssignmentTab(row){
-        console.log(JSON.stringify(event));
-        console.log('Row clicked:', row);
-        var tab = new ContentPane({title: "Assignment: "+row.data.topic, closable:true});
-        var namecp = new ContentPane({content:"Name:"});
-        var nametb = new ValidationTextBox();
-        namecp.addChild(nametb);
-        tab.addChild(namecp);
-        return tab;
-    }
-    
-    function createBorderContainer(){
-        var borderContainer = new BorderContainer({style: "height: 100%; width:100%;", liveSplitters:true, gutter:true});
-        borderContainer.addChild(createMainMenuBar());
-        var tabContainer = new TabContainer({region: "center"});
-        borderContainer.addChild(tabContainer);
-        var tree = createTreeMenu();
-        tree.onClick = function(item, node, event){
-                if(item.id==='all_assignments' && !window.allAssignmentsTabOpen){
-                    var grid = createAllAssignmentsGrid();
-                    
-                    grid.on('.dgrid-row:dblclick', function (event) {
-                        tabContainer.addChild(openEditAssignmentTab(grid.row(event)));
-                    });
-                
-                    let tab = openAllAssignmentsTab();
-                    tab.addChild(grid);
-                    tabContainer.addChild(tab);
-                    grid.startup();
-                }
-                console.log(node+" "+JSON.stringify(item)+" "+event);
-            }
-        borderContainer.addChild(tree);
-        document.body.appendChild(borderContainer.domNode);
-        borderContainer.startup();
-    };
-	
     function createTreeMenu(){
         var treeMenuMemory = new Memory({
             data: [
@@ -107,65 +123,110 @@ define([
             showRoot: false
         });
         
-        return tree;
-    }
+        tree.onClick = function(item, node, event){
+            if(item.id==='all_assignments' && !kernel.global.allAssignmentsTabOpen)
+                openAllAssignmentsTab();
+            if(item.id==='my_assignments')
+                openAllAssignmentsTab();
+            if(item.id==='for_me_assignments')
+                openAllAssignmentsTab();
+            if(item.id==='organizations')
+                openAllAssignmentsTab();
+            if(item.id==='employees')
+                openAllAssignmentsTab();
+        }
         
-    function createGrid(apitarget, gridcolumns){
-        var assignmentData = new RequestMemory({ target: apitarget });
-        var grid = new OnDemandGrid({
-            collection: assignmentData,
-            columns: gridcolumns,
-            loadingMessage: 'Loading data...',
-            noDataMessage: 'No results found.'
+        kernel.global.mainBorderContainer.addChild(tree);
+    }
+
+    function openAllAssignmentsTab(){  
+        if(kernel.global.allAssignmentsTabOpen) return;
+        
+        var tab = new ContentPane({
+            title: "All assignments",
+            closable: true,
+            onClose: function(){
+                kernel.global.allAssignmentsTabOpen = false;
+            }
         });
-        return grid;
+        tab.addChild(new Button({
+            label: "Add assignment",
+            onClick: function(){openEditAssignmentTab()}
+        }));
+        tab.addChild(new Button({
+            label: "Edit assignment",
+            onClick: function(){openEditAssignmentTab()}
+        }));
+        tab.addChild(new Button({
+            label: "Delete assignment",
+            onClick: function(){openEditAssignmentTab()}
+        }));
+        
+        var grid = createAllAssignmentsGrid();
+        grid.on('.dgrid-row:click', function (event) {
+            kernel.global.mainTabContainerSelectedRow = grid.row(event);
+        });
+        grid.on('.dgrid-row:dblclick', function (event) {
+            openEditAssignmentTab(grid.row(event));
+        });
+        tab.addChild(grid);
+        kernel.global.mainTabContainer.addChild(tab);
+        
+        grid.startup();
+    }
+    
+    function openDeleteAssignmentDialog(){
+        if(kernel.global.mainTabContainerSelectedRow===undefined) return;
+    }
+
+    function openEditAssignmentTab(row){
+        if(row===undefined){
+            if(kernel.global.mainTabContainerSelectedRow===undefined) return;
+            row = kernel.global.mainTabContainerSelectedRow;
+        }
+        var tab = new ContentPane({title: "Edit assignment: "+row.data.topic, closable:true});
+        
+        var cp = new ContentPane({content:"Topic:"});
+        var tb = new TextBox();
+        cp.addChild(tb);
+        tab.addChild(cp);
+        
+        var cp = new ContentPane({content:"Text:"});
+        var tb = new TextBox();
+        cp.addChild(tb);
+        tab.addChild(cp);
+        
+        var cp = new ContentPane({content:"Text:"});
+        var tb = new TextBox();
+        cp.addChild(tb);
+        tab.addChild(cp);
+        
+        tab.addChild(new Button({
+            label: "Save",
+            onClick: function(){}
+        }));
+        
+        kernel.global.mainTabContainer.addChild(tab);
     }
     
     function createAllAssignmentsGrid(){
-        var grid = createGrid('api/assignment/find_all', [
-                { field: 'id', label: 'ID'},
+        var assignmentData = new RequestMemory({ target: 'api/assignment/find_all' });
+        var grid = new OnDemandGrid({
+            collection: assignmentData,
+            columns: [{ field: 'id', label: 'ID'},
                 { field: 'topic', label: 'Topic' },
                 { field: 'text', label: 'Text'},
                 { field: 'author', label: 'Author', 
                     formatter: function (author) {
                             return author.lastname + " " + author.firstname;
                     }
-                },
-            ]);
+                }],
+            loadingMessage: 'Loading data...',
+            noDataMessage: 'No results found.'
+        });
         return grid;
     }
-        
-    function openAllAssignmentsTab(){   
-        var tab = new ContentPane({
-            title: "All assignments",
-            onClose: function(){
-                window.allAssignmentsTabOpen = false;
-            }
-        });
-        var addbut = new Button({
-            label: "Add assignment",
-            onClick: function(){
-                console.log("add");
-            }
-        });
-        tab.addChild(addbut);
-        var editbut = new Button({
-            label: "Edit assignment",
-            onClick: function(){
-                console.log("edit");
-            }
-        });
-        tab.addChild(editbut);
-        var delbut = new Button({
-            label: "Delete assignment",
-            onClick: function(){
-                console.log("add");
-            }
-        });
-        tab.addChild(delbut);
-        return tab;
-    }
-    
+      
     createBorderContainer();
     
 });
